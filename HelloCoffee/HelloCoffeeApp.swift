f7a5833ab56cb7e597eaadcbd799c420c969f79f
Thiago Pires <thiagopac@gmail.com>
2026-02-11 14:16:39 -0300

    Initial Commit

---
+++ HelloCoffee/HelloCoffeeApp.swift
@@ -0,0 +1,18 @@
+//
+//  HelloCoffeeApp.swift
+//  HelloCoffee
+//
+//  Created by Thiago Castro on 11/02/26.
+//
+
+
+import SwiftUI
+
+@main
+struct HelloCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
